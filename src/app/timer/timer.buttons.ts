7f64842c57ff0fb5a690eb7dba5194fafeaa7d6c
Oishe <farhanoishe@gmail.com>
2019-01-24 16:05:35 -0500

    Firebase deployment

--- src/app/timer/timer.buttons.ts
+++ src/app/timer/timer.buttons.ts
@@ -5,21 +5,26 @@ import { RecorderService } from '../recorder/recorder.service';
 @Component({
   selector: 'timer-buttons',
   templateUrl: './timer.buttons.html',
-  styleUrls: ['./timer.buttons.css']
+  styleUrls: ['./timer.buttons.css'],
 })
-
 export class ButtonsComponent implements OnInit, OnDestroy {
-
   private playStopUnsubscribe: any;
   private playStartUnsubscribe: any;
-  private play: boolean;
+  public play: boolean;
   private playStart: boolean;
 
-  constructor(private timerService: TimerService, private recording: RecorderService) {}
+  constructor(
+    private timerService: TimerService,
+    private recording: RecorderService
+  ) {}
 
   ngOnInit() {
-    this.playStopUnsubscribe = this.timerService.playStop$.subscribe((res: any) => this.setPlay(res));
-    this.playStartUnsubscribe = this.recording.startStop$.subscribe((res: any) => this.setPlayStart(res));
+    this.playStopUnsubscribe = this.timerService.playStop$.subscribe(
+      (res: any) => this.setPlay(res)
+    );
+    this.playStartUnsubscribe = this.recording.startStop$.subscribe(
+      (res: any) => this.setPlayStart(res)
+    );
   }
 
   ngOnDestroy() {
@@ -28,29 +33,26 @@ export class ButtonsComponent implements OnInit, OnDestroy {
   }
 
   private setPlay(res: any) {
-    (res.play) ? this.play = true : this.play = false;
+    res.play ? (this.play = true) : (this.play = false);
   }
 
   private setPlayStart(res: any) {
-    (res.start) ? this.playStart = true : this.playStart = false;
+    res.start ? (this.playStart = true) : (this.playStart = false);
   }
 
   playTimer() {
-    if (!this.play){
+    if (!this.play) {
       this.timerService.playTimer();
       this.recording.startRecording();
       this.playStart = true;
-    }
-    else alert("Timer must be stopped before starting a new recording");
+    } else alert('Timer must be stopped before starting a new recording');
   }
 
   stopTimer() {
-    if (this.play){
+    if (this.play) {
       this.timerService.stopTimer();
       this.recording.stopRecording();
       this.playStart = false;
-    }
-    else alert("Timer must be started before ending a recording");
+    } else alert('Timer must be started before ending a recording');
   }
-
 }
