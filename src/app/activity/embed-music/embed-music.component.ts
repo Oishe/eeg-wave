efe36b2458ac5aba82e23ceb877d572dcfd32e3f
Oishe <farhanoishe@gmail.com>
2019-01-30 11:54:43 -0500

    8 tracks and play back

--- src/app/activity/embed-music/embed-music.component.ts
+++ src/app/activity/embed-music/embed-music.component.ts
@@ -6,16 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./embed-music.component.css'],
 })
 export class EmbedMusicComponent implements OnInit {
-  Songs: String[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  Songs: String[] = [
+    '0-94stones-limit_break_x_survivor',
+    '1-ashes_remain-without_you',
+    '2-blackbear-do_re_mi',
+    '3-khalid-suncity_ft_empress_of',
+    '4-marshmello-alone',
+    '5-porter_robinson-shelter',
+    '6-shakira-chantaje_ft_maluma',
+    '7-verzache-waiting_for_you',
+  ];
   selectedSong = '';
   audio: any;
   constructor() {}
 
-  ngOnInit() {
-    this.audio = new Audio('../../../assets/sounds/BrownNoise30secs.mp3');
-  }
+  ngOnInit() {}
 
   playBrownNoise() {
+    try {
+      this.audio.pause();
+    } catch (error) {}
+    this.audio = new Audio(
+      '../../../assets/sounds/' + this.selectedSong + '.mp3'
+    );
     this.audio.play();
   }
 }
